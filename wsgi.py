71f15fdde2c2b3d757bfa7765be9eba754d14e2e
mdecourse <mdecourse@gmail.com>
2020-03-11 10:34:31 +0800

    use opensssl generate self-signed certificate to allow https connection

--- wsgi.py
+++ wsgi.py
@@ -4,14 +4,21 @@
 import os
 # 導入同目錄下的 myflaskapp.py
 import myflaskapp
+import ssl
+
+# 即使在近端仍希望以 https 模式下執行
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('cd2020.crt', 'cd2020.key')
+
+uwsgi = False
 # 以下一行為原先在 OpenShift 平台執行時的單行程式
 #from myflaskapp import app as application
 # 以下開始判斷在 OpenShift 或近端執行
-if 'OPENSHIFT_REPO_DIR' in os.environ.keys():
+if uwsgi:
     # 表示程式在雲端執行
     application = myflaskapp.app
 else:
     # 表示在近端執行
-    myflaskapp.app.run(debug=True)
+    myflaskapp.app.run(host='127.0.0.1', port=8443, debug=True, ssl_context=context)
     
 

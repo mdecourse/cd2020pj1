6c7cece2f317123bf16381c230dd446f4f28698a
mdecourse <mdecourse@gmail.com>
2020-04-09 15:04:03 +0800

    change all comments to English

--- myflaskapp.py
+++ myflaskapp.py
@@ -16,11 +16,11 @@ def send_static(path):
     return send_from_directory('static', path)
 @app.route("/")
 def index():
-    #這是猜數字遊戲的起始表單, 主要在產生答案, 並且將 count 歸零
-    # 將標準答案存入 answer session 對應區
+    # This is the starting form for guessing numbers game, mainly to generate answers, and to reset count to zero
+    # Store answers of guessing
     theanswer = random.randint(1, 100)
     thecount = 0
-    # 將答案與計算次數變數存進 session 對應變數
+    # Store the answer and the number of calculation variables in the session
     session['answer'] = theanswer
     session['count'] = thecount
 
@@ -36,7 +36,7 @@ def req1():
     return '<p>Your browser is %s</p>' % user_agent
 @app.route('/red')
 def red():
-    # 重新導向 google
+    # redirect to google
     return redirect("http://www.google.com")
 @app.route('/guessform')
 def guessform():
@@ -47,36 +47,37 @@ def guessform():
     return render_template("guessform.html", guess=guess, answer=theanswer, count=count)
 @app.route('/docheck', methods=['POST'])
 def docheck():
-    # session[] 存資料
-    # session.get() 取 session 資料
-    # 利用 request.form[] 取得表單欄位資料, 然後送到 template
+    # use session[] to save data
+    # use session.get() to get session data
+    # use request.form[] to get field data of form and send to template
     guess = request.form["guess"]
     session["guess"] = guess
-    # 假如使用者直接執行 doCheck, 則設法轉回根方法
+    # if use execute doCheck directly, send it back to the root method
     if guess is None:
         redirect("/")
-    # 從 session 取出 answer 對應資料, 且處理直接執行 docheck 時無法取 session 值情況
+    # get answer from session, when execute doCheck directly, no session data will be accessed
     try:
         theanswer = int(session.get('answer'))
     except:
         redirect("/")
-    # 經由表單所取得的 guess 資料型別為 string
+    # the data type from web based form is string
     try:
         theguess = int(guess)
     except:
         return redirect("/guessform")
-    # 每執行 doCheck 一次,次數增量一次
+    # every doCheck being executed increase the count session value
     session["count"] += 1
     count = session.get("count")
-    # 答案與所猜數字進行比對
+    # compare the answer and the guess value
     if theanswer < theguess:
         return render_template("toobig.html", guess=guess, answer=theanswer, count=count)
     elif theanswer > theguess:
         return render_template("toosmall.html", guess=guess, answer=theanswer, count=count)
     else:
-        # 已經猜對, 從 session 取出累計猜測次數
+        # 
+        # got the answer, get count from session
         thecount = session.get('count')
-        return "猜了 "+str(thecount)+" 次, 終於猜對了, 正確答案為 "+str(theanswer)+": <a href='/'>再猜</a>"
+        return "Guess "+str(thecount)+" times, finally got the answer, the answer is "+str(theanswer)+": <a href='/'>Play again</a>"
     return render_template("docheck.html", guess=guess)
  
 

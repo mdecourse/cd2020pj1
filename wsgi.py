f53d1fc6ad4f8b876865968818813b45f4d3fead
mdecourse <mdecourse@gmail.com>
2020-04-19 23:42:26 +0800

    try to use Cango js library to draw spur gear outline

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 # https://mde.tw/cd2020 協同設計專案
 #!/usr/bin/python
-# 導入 os 模組, 主要用來判斷是否在 OpenShift 上執行
+# 導入 os 模組, 主要用來判斷是否在遠端或近端上執行
 import os
 # 導入同目錄下的 myflaskapp.py
 import myflaskapp
@@ -11,9 +11,8 @@ context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain('cd2020.crt', 'cd2020.key')
 
 uwsgi = False
-# 以下一行為原先在 OpenShift 平台執行時的單行程式
-#from myflaskapp import app as application
-# 以下開始判斷在 OpenShift 或近端執行
+
+# 以下開始判斷在遠端或近端執行
 if uwsgi:
     # 表示程式在雲端執行
     application = myflaskapp.app

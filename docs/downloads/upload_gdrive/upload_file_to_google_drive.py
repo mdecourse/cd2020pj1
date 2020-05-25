e13e7c4327edd498ee5f3a9c1cf8e1947fc3f509
mdecourse <mdecourse@gmail.com>
2020-05-25 14:56:15 +0800

    add file upload to GDrive example

--- docs/downloads/upload_gdrive/upload_file_to_google_drive.py
+++ docs/downloads/upload_gdrive/upload_file_to_google_drive.py
@@ -3,10 +3,9 @@ from pydrive.drive import GoogleDrive
 
 gauth = GoogleAuth()
 gauth.LoadClientConfigFile("./../client_secrets.json")
-gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
 drive = GoogleDrive(gauth)
 
-
+'''
 # View all folders and file in your Google Drive
 fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 for file in fileList:
@@ -15,10 +14,17 @@ for file in fileList:
   # 檔案會上傳到根目錄下的 uploaded  目錄中
   if(file['title'] == "uploaded"):
       fileID = file['id']
+'''
+# GDrive 上 uploaded 目錄的 fileID
+with open("./../uploaded_id.txt", 'r') as content_file:
+    fileID = content_file.read()
+
+#fileID = "your_folder_file_ID"
 
 fileName = "ModernC.pdf"
 filePath = "./test/"
 file1 = drive.CreateFile({"mimeType": "application/pdf", "parents": [{"kind": "drive#fileLink", "id": fileID}], "title":  fileName})
 file1.SetContentFile(filePath + fileName)
 file1.Upload() # Upload the file.
-print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))   
+#print('Created file %s with mimeType %s' % (file1['title'], file1['mimeType']))   
+print("upload fileID:" + str(file1['id']))
